e4cb4f7c82fe9231ae80c30ce380119625f520dc
Gabriel Vitor Ferreira do Amaral <gabriel.vitor@totvs.com.br>
2023-04-12 19:37:37 -0300

    seção 7

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para criar Migrações
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ src/Api.Data/Migrations/20230411161000_UserMigration2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class UserMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "_updateAt",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateAt",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdateAt",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "_updateAt",
+                table: "User",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
